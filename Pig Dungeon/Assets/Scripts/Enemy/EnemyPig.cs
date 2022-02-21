1d5c1a7e3d3be641c942f74a10a504cb9c737adc
Chakujin <cristiangalindo13@gmail.com>
2022-02-21 10:45:57 +0100

    pigmove

--- Pig Dungeon/Assets/Scripts/Enemy/EnemyPig.cs
+++ Pig Dungeon/Assets/Scripts/Enemy/EnemyPig.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class EnemyPig : EnemyClass
 {
+    public float speed;
     public float detectRange;
     public LayerMask playerLayer;
     private bool b_startAttack = false;
 
     public Transform detectedPoint;
+    public Transform[] PointMove;
+    private int i_currentPoint;
+    private Vector2 v_moveDirection;
+    private float f_lastDicrection;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +24,28 @@ public class EnemyPig : EnemyClass
     // Update is called once per frame
     void Update()
     {
-
+        if (Vector2.Distance(transform.position, PointMove[i_currentPoint].transform.position) < 0.1 )
+        {
+            i_currentPoint++;
+            i_currentPoint %= PointMove.Length;
+            //StopMove
+        }
+        else
+        {
+            v_moveDirection = transform.position + PointMove[i_currentPoint].position;
+            transform.position = Vector2.MoveTowards(transform.position, PointMove[i_currentPoint].transform.position, Time.deltaTime * speed);
+            /*
+            if(v_moveDirection.x - transform.position.x < transform.position.x)
+            {
+                Debug.Log("derecha");
+            }
+            else (v_moveDirection.x - transform.position.x > transform.position.x)
+            {
+                Debug.Log("izquierda");
+            }
+            */
+        }
     }
-
     private void FixedUpdate()
     {
         Collider2D[] hitEnemyes = Physics2D.OverlapCircleAll(detectedPoint.position, detectRange, playerLayer);
@@ -41,7 +65,6 @@ public class EnemyPig : EnemyClass
     {
         EnemyAnimator.SetTrigger("Attack");
         playerDetected.GetComponentInParent<PlayerMovement>().TakeDamage(1);
-
 
         //playerDetected.GetComponent<Rigidbody2D>().AddForce(new Vector2(1,1));
 
---
+++ Pig Dungeon/Assets/Scripts/SpacePointMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpacePointMove : MonoBehaviour
+{
+    public GameObject[] mySpacePoint;
+    public GameObject pigTarget;
+    private Transform f_startpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        f_startpos = pigTarget.transform;
+        foreach (GameObject pos in mySpacePoint)
+        {
+            pos.transform.position = new Vector2(pos.transform.position.x, f_startpos.transform.position.y);
+        }
+    }
+}
