5f141f5798f9abd8f4e488ff2c35d0e4d5cf8ba6
Chakujin <cristiangalindo13@gmail.com>
2022-03-01 16:13:55 +0100

    Start PigDropBox

---
+++ Pig Dungeon/Assets/Scripts/Enemy/EnemyPigDropBox.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPigDropBox: EnemyClass
+{
+    public Transform detectRigth;
+    public Transform detectLeft;
+    public Transform detectedPoint;
+    public Transform spawnBox;
+    public Transform spawnBoxInverted;
+
+    public GameObject Box;
+    public LayerMask playerLayer;
+    public Vector2 sizeDetectors;
+    public Vector2 detectRange;
+
+    public float reloadtime;
+    private bool b_startAttack = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth += maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        Collider2D[] hitEnemyes = Physics2D.OverlapBoxAll(detectedPoint.position, detectRange, playerLayer);
+        Collider2D[] detectRigthPlayer = Physics2D.OverlapBoxAll(detectRigth.position, sizeDetectors, 0, playerLayer);
+        Collider2D[] detectLeftPlayer = Physics2D.OverlapBoxAll(detectLeft.position, sizeDetectors, 0, playerLayer);
+
+        if (b_startAttack == false && die == false)
+        {
+
+            foreach (Collider2D playerRigth in detectRigthPlayer)
+            {
+                detectHitRigth = true;
+                detectHitLeft = false;
+                //Detect Player Rigth
+                foreach (Collider2D player in hitEnemyes)
+                {
+                    if (b_startAttack == false)
+                    {
+                        b_startAttack = true;
+                        StartCoroutine(startAttack());
+                        mySpriteRenderer.flipX = false;
+                        move = false;
+                    }
+                }
+            }
+
+            foreach (Collider2D playerLeft in detectLeftPlayer)
+            {
+                detectHitRigth = false;
+                detectHitLeft = true;
+                //Detect Player Left
+                foreach (Collider2D player in hitEnemyes)
+                {
+                    if (b_startAttack == false)
+                    {
+                        b_startAttack = true;
+                        StartCoroutine(startAttack());
+                        mySpriteRenderer.flipX = true;
+                        move = false;
+                    }
+                }
+            }
+        }
+    }
+
+    private IEnumerator startAttack()
+    {
+        EnemyAnimator.SetTrigger("Attack");
+
+        if (detectHitLeft == true)
+        {
+            mySpriteRenderer.flipX = false;
+        }
+        else if (detectHitRigth == true)
+        {
+            mySpriteRenderer.flipX = true;
+        }
+
+        yield return new WaitForSeconds(0.2f);
+        EnemyAnimator.SetTrigger("Reload");
+
+        if (detectHitLeft == true)
+        {
+            GameObject mybomb = Instantiate(Box, spawnBoxInverted);
+            mybomb.GetComponent<Rigidbody2D>().AddForce(new Vector2(4, 0), ForceMode2D.Impulse);
+        }
+        else if (detectHitRigth == true)
+        {
+            GameObject mybomb = Instantiate(Box, spawnBox);
+            mybomb.GetComponent<Rigidbody2D>().AddForce(new Vector2(-4, 0), ForceMode2D.Impulse);
+        }
+
+        yield return new WaitForSeconds(reloadtime);
+        b_startAttack = false;
+    }
+
+    //Draw Gizmos
+    private void OnDrawGizmosSelected()
+    {
+        if (detectedPoint == null)
+            return;
+
+        Gizmos.DrawWireCube(detectedPoint.position, new Vector3(detectRange.x, detectRange.y, 1));
+        Gizmos.DrawWireCube(detectRigth.position, new Vector3(sizeDetectors.x, sizeDetectors.y, 1));
+        Gizmos.DrawWireCube(detectLeft.position, new Vector3(sizeDetectors.x, sizeDetectors.y, 1));
+    }
+}
---
+++ Pig Dungeon/Assets/Scripts/Props/BoxDroped.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxDroped : MonoBehaviour
+{
+    public Animator myAnimator;
+    public GameObject box;
+    public GameObject[] pieces;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+
+        }
+        if(collision.tag == "Background")
+        {
+            for(int i = 0; i < pieces.Length; i++)
+            {
+                pieces[i].SetActive(true);
+
+                float forcex = Random.Range(-3,3);
+                float forcey = Random.Range(0,3);
+
+                pieces[i].GetComponent<Rigidbody2D>().AddForce(new Vector2(forcex,forcey), ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    private IEnumerator hitPlayer;
+}
