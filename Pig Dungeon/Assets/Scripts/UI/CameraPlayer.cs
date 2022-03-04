a1079b7af2513d9870817686a120b3b63fcf9b42
Chakujin <cristiangalindo13@gmail.com>
2022-03-04 02:03:19 +0100

    virtual camera add

---
+++ Pig Dungeon/Assets/Scripts/UI/CameraPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraPlayer : MonoBehaviour
+{
+    public static CameraPlayer Instance { get; private set; }
+    private CinemachineVirtualCamera m_cinemachineVirtualCamera;
+    private float shakeTimer;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Instance = this;
+        m_cinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    void Update()
+    {
+        if(shakeTimer > 0)
+        {
+            shakeTimer -= Time.deltaTime;
+            if (shakeTimer <= 0f)
+            {
+                //Time over
+                CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin =
+                    m_cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+                cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 0f;
+            }
+        }
+    }
+    
+    public void ShakeCamera(float intensity, float time)
+    {
+        CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin =
+            m_cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = intensity;
+        shakeTimer = time;
+    }
+}
