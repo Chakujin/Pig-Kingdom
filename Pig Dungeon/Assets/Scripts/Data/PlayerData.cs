a6653ad5521d8c919f89d686542be46e3ab60547
Chakujin <cristiangalindo13@gmail.com>
2022-03-05 20:06:11 +0100

    save and load system

---
+++ Pig Dungeon/Assets/Scripts/Data/PlayerData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    public int heal;
+    public int diamonds;
+    public int level;
+
+    public PlayerData(GameManager manager)
+    {
+        diamonds = manager.playerDiamond;
+        heal = manager.playerHeal;
+        level = manager.level;
+
+    }
+}
---
+++ Pig Dungeon/Assets/Scripts/Data/SaveSystem.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem
+{
+    public static void saveGame(GameManager gameManager)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/player.fun";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerData data = new PlayerData(gameManager);
+
+        formatter.Serialize(stream,data);
+        stream.Close();
+    }
+
+    public static PlayerData loadGame()
+    {
+        string path = Application.persistentDataPath + "/player.fun";
+
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            Debug.LogError("Save file dont found in" + path);
+            return null;
+        }
+    }
+}
