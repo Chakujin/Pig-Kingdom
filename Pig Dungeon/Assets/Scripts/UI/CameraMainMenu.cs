8f2df76aff70be3664d0565afe3da453d5797b9e
Chakujin <cristiangalindo13@gmail.com>
2022-03-08 13:37:58 +0100

    start create level 1

---
+++ Pig Dungeon/Assets/Scripts/UI/CameraMainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using DG.Tweening;
+
+public class CameraMainMenu : MonoBehaviour
+{
+    public CinemachineVirtualCamera myCamera;
+    public float positionMove1;
+    public float positionMove2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Change()
+    {
+        myCamera.transform.DOMoveX(positionMove1, 1f).SetEase(Ease.InSine).OnComplete(Change2);
+    }
+    private void Change2()
+    {
+        myCamera.transform.DOMoveX(positionMove2, 1f).SetEase(Ease.InSine).OnComplete(Change);
+    }
+}
