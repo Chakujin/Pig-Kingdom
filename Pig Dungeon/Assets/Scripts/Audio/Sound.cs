9f171b7f24b14df2faa9ee1694b6a42463bb26c8
Chakujin <cristiangalindo13@gmail.com>
2022-03-13 23:01:22 +0100

    audiomanager + pausemanager

---
+++ Pig Dungeon/Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+
+    public AudioClip clip;
+    public AudioMixer audioMixer;
+
+    public float minPitch;
+    public float maxPitch;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(-3f, 3f)]
+    public float pitch;
+    public string outputMixer;
+    public float spatialBlend;
+    public AudioRolloffMode rolloffMode;
+    public float dopplerLevel;
+    public float maxDistance;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+
+}
