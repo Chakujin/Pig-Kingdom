9abe1f1656a2f54e81e5c5d1903f153f976ce801
Chakujin <cristiangalindo13@gmail.com>
2022-03-29 19:39:18 +0200

    CameraShake Main menu

--- Pig Dungeon/Assets/Scripts/UI/CameraMainMenu.cs
+++ Pig Dungeon/Assets/Scripts/UI/CameraMainMenu.cs
@@ -6,22 +6,24 @@ using DG.Tweening;
 
 public class CameraMainMenu : MonoBehaviour
 {
-    public CinemachineVirtualCamera myCamera;
+    public Camera myCamera;
     public float positionMove1;
     public float positionMove2;
+    public float time;
+    public Ease easing;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Change();
     }
 
     private void Change()
     {
-        myCamera.transform.DOMoveX(positionMove1, 1f).SetEase(Ease.InSine).OnComplete(Change2);
+        myCamera.transform.DOMoveX(positionMove1, time).SetEase(easing).OnComplete(Change2);
     }
     private void Change2()
     {
-        myCamera.transform.DOMoveX(positionMove2, 1f).SetEase(Ease.InSine).OnComplete(Change);
+        myCamera.transform.DOMoveX(positionMove2, time).SetEase(easing).OnComplete(Change);
     }
 }
